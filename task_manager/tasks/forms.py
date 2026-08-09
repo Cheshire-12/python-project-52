38e0f900719de72da83d6667597b9788e98e15c7
Andrey Vedenkin <andr.vnk@gmail.com>
2026-08-09 16:24:23 +0300

    minor fix

--- task_manager/tasks/forms.py
+++ task_manager/tasks/forms.py
@@ -1,15 +1,23 @@
 from django import forms
+from django.contrib.auth import get_user_model
 
 from task_manager.labels.models import Label
 
 from .models import Task
 
+User = get_user_model()
 
 class TaskForm(forms.ModelForm):
     labels = forms.ModelMultipleChoiceField(
         queryset=Label.objects.all(),
         required=False,
         label=('Labels')
+    )
+    
+    executor = forms.ModelChoiceField(
+        queryset=User.objects.all(),
+        required=False,
+        label=('Executor'),
     )
 
     class Meta:
